79b11d79373412e734e6f41521526cc82cba502d
MatthieuM <matthieu.masselin@wandercraft.eu>
2015-10-17 12:12:30 +0200

    Add Strava part, global structure and script plan

---
+++ Strava_retriever.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+# First start a webdriver
+
+# Then connect to strava website
+
+# Connect to account
+
+# Navigate to activities history
+
+# Retrieve history and in particular last activity
