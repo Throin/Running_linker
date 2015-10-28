bd3a74782850960732dcf731b500ba27d2cbcdb6
MatthieuM <matthieu.masselin@wandercraft.eu>
2015-10-28 20:15:19 +0100

    New way to go through the activities and dl them if later than reference date

--- Export_runtastic.py
+++ Export_runtastic.py
@@ -7,6 +7,8 @@ import os
 from personal import runtastic_pw, runtastic_login
 from CustDate import CustDate
 
+month_conversion = {"Jan": 1, u'F\xe9vr.': 2, "Mars": 3, "Avr": 4, "Mai": 5, "Juin": 6, "Juil": 7, u'Ao\xfbt': 8, "Sept": 9, "Oct": 10, "Nov": 11, u'D\xe9c': 12}
+
 passed_args = sys.argv
 date_components = passed_args[1:]
 for s in sys.argv:
@@ -21,27 +23,14 @@ if last_strava_activity:
 	print last_strava_activity
 	
 
+# "_".join(time.asctime().replace(":", "_").split())
+
 profile = webdriver.FirefoxProfile()
 profile.set_preference("browser.download.folderList", 2)
 profile.set_preference("browser.download.dir", os.getcwd())
 profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/gpx+tcx");
 profile.set_preference("browser.helperApps.alwaysAsk.force", False);
 profile.set_preference("browser.download.manager.showWhenStarting", False);
-
-# profile.set_preference("browser.download.panel.shown", False)
-# profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/pdf;application/octet-stream")
-# profile.set_preference("browser.helperApps.alwaysAsk.force", False);
-# profile.set_preference("browser.download.manager.showWhenStarting", False)
-
-# profile.set_preference("browser.download.alertOnEXEOpen", False);
-# profile.set_preference("browser.download.manager.focusWhenStarting", False);
-# profile.set_preference("browser.download.manager.alertOnEXEOpen", False);
-# profile.set_preference("browser.download.manager.closeWhenDone", False);
-# profile.set_preference("browser.download.manager.showAlertOnComplete", False);
-# profile.set_preference("browser.download.manager.useWindow", False);
-# profile.set_preference("browser.download.manager.showWhenStarting", False);
-# profile.set_preference("services.sync.prefs.sync.browser.download.manager.showWhenStarting", False);
-# profile.set_preference("pdfjs.disabled", True);
 
 
 driver = webdriver.Firefox(firefox_profile=profile)
@@ -111,29 +100,52 @@ while(still_unimported):
 				found_date = CustDate(year = year_infos, month = month_infos, day = day_infos)
 				if found_date > last_strava_activity:
 					print "activity to add, dated: ", found_date
+					# The following opens a new tab, which originally was supposed to be better for navigation - one new tab for each activity. This is no longer what's considered but we keep the original "new window"
 					curr_w_h = driver.current_window_handle
 					cand.send_keys(Keys.CONTROL + Keys.RETURN)
 					driver.find_element_by_tag_name("body").send_keys(Keys.CONTROL + Keys.TAB)
 					driver.switch_to_window(curr_w_h) # switch focus to current window, ie. the window that has just been opened
-					## TODO: dl the gps trace
-					options_buttons = driver.find_elements_by_id("show_more_options")
-					for btn in options_buttons:
-						if btn.get_attribute("class") == "" and btn.is_displayed():
-							btn.click()
-							childs = btn.find_elements_by_xpath(".//a")
-							for elem in childs:
-								if "charger" in elem.text and elem.is_displayed():
-									print "let's dl"
-									elem.click()
-									driver.find_elements_by_partial_link_text(".tcx")[0].click()
-					
-				## TODO: process info to see if we are 
-			else:
-				pass
-				# print "y:",  "year" in raw_infos, " / m:", "month" in raw_infos, " / d:", "day" in raw_infos, " /id:", "id" in raw_infos	
-	
+					break 
 	## TODO: decide if we have to keep on browsing history - and in that case actually browse it - or if we have reached the end
 	still_unimported = False
+	
+print "out of the break"
+
+can_go_later = True
+while(can_go_later):
+	right_nav_buttons = driver.find_elements_by_class_name("nav_right")
+	if right_nav_buttons:
+		right_nav_buttons[0].click()
+	else:
+		can_go_later = False
+
+del can_go_later
+
+date_in_range = True
+while (date_in_range):
+	act_date_components = driver.title.split("(")[1].split("|")[0].split() # Warning: this is heavily dependant on runtastic way of displaying things
+	year_current = int(act_date_components[2])
+	processed_month = act_date_components[1].strip(",")
+	month_current = month_conversion[processed_month]
+	day_current = int(act_date_components[0])
+	act_date = CustDate(year = year_current, month = month_current, day = day_current)
+	if act_date > last_strava_activity:
+		options_buttons = driver.find_elements_by_id("show_more_options")
+		for btn in options_buttons:
+			if btn.get_attribute("class") == "" and btn.is_displayed():
+				btn.click()
+				childs = btn.find_elements_by_xpath(".//a")
+				for elem in childs:
+					if "charger" in elem.text and elem.is_displayed():
+						print "let's dl"
+						elem.click()
+						driver.find_elements_by_partial_link_text(".tcx")[0].click()
+						driver.find_elements_by_class_name("nav_left")[0].click()
+						break ##TODO: get out of the multiple loops properly 
+	else:
+		date_in_range = False
+				
+
 	
 
 ########## This is the code graveyard, in order to come back from the dead when the time shall be right ########## 
@@ -159,6 +171,21 @@ while(still_unimported):
 ##            if new_elem.get_attribute("type") and 'submit' in new_elem.get_attribute("submit"):
 ##                press_enter_button = elem
 ##        press_enter_button.click()
+## profile.set_preference("browser.download.panel.shown", False)
+## profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/pdf;application/octet-stream")
+## profile.set_preference("browser.helperApps.alwaysAsk.force", False);
+## profile.set_preference("browser.download.manager.showWhenStarting", False)
+
+## profile.set_preference("browser.download.alertOnEXEOpen", False);
+## profile.set_preference("browser.download.manager.focusWhenStarting", False);
+## profile.set_preference("browser.download.manager.alertOnEXEOpen", False);
+## profile.set_preference("browser.download.manager.closeWhenDone", False);
+## profile.set_preference("browser.download.manager.showAlertOnComplete", False);
+## profile.set_preference("browser.download.manager.useWindow", False);
+## profile.set_preference("browser.download.manager.showWhenStarting", False);
+## profile.set_preference("services.sync.prefs.sync.browser.download.manager.showWhenStarting", False);
+## profile.set_preference("pdfjs.disabled", True);
+
 
 # Log-in
 # login_field = driver.find_element_by_name("login")
