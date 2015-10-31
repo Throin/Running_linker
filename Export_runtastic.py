7d97e5215239d2c8297510bda488846aad175b59
MatthieuM <matthieu.masselin@wandercraft.eu>
2015-10-31 11:51:16 +0100

    re-architectured module so that different parts of the code can be called independently when importing module

--- Export_runtastic.py
+++ Export_runtastic.py
@@ -23,145 +23,153 @@ def browse_buttons_and_navigate(driver):
 					driver.find_elements_by_class_name("nav_left")[0].click()
 					return
 					
-passed_args = sys.argv
-date_components = passed_args[1:]
-for s in sys.argv:
-#    print s
-	pass
+def instantiate_auto_download_browser()	:
+	dl_dir = os.getcwd() + "\\" +"_".join(time.asctime().replace(":", "_").split())
+	# print dl_dir 
+
+	profile = webdriver.FirefoxProfile()
+	profile.set_preference("browser.download.folderList", 2)
+	profile.set_preference("browser.download.dir", dl_dir)
+	profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/gpx+tcx");
+	profile.set_preference("browser.helperApps.alwaysAsk.force", False);
+	profile.set_preference("browser.download.manager.showWhenStarting", False);
+
+	driver = webdriver.Firefox(firefox_profile=profile)
+	driver.get("https://www.runtastic.com")
+	return driver
+
+def log_onto_runtastic(driver):
+
+	# Detect "connect" button
+	connect_btn_candidates = driver.find_elements_by_class_name("bttn")
+	for candidate in connect_btn_candidates:
+		if "connect" in candidate.text and candidate.is_displayed():
+			candidate.click()
+			break
+
+	########## Go through first page and connect to account ########## 
+	# Detect fields to fill : email input field and password input field
+	input_fields = driver.find_elements_by_class_name("js-input")
+	mail_field_found = False
+	pw_field_found = False
+	for elem in input_fields:
+		parent = elem.find_element_by_xpath("..")
+		parent_class = parent.get_attribute("class")
+		if "email" in parent_class and "login" in parent_class:
+			mail_input_field = elem
+			mail_field_found = True
+		elif "password" in parent_class and "login" in parent_class:
+			pw_input_field = elem
+			pw_field_found = True
+		else:
+			print ""
+
+	# Fill fields and connect to session
+	if mail_field_found and pw_field_found:
+		mail_input_field.send_keys(runtastic_login)
+		pw_input_field.send_keys(runtastic_pw)
+		pw_input_field.send_keys(Keys.RETURN)
 	
-last_strava_activity = 0
-if len(date_components) == 3:
-	last_strava_activity = CustDate(year = int(date_components[0]), month = int(date_components[1]), day = int(date_components[2]))
+def navigate_to_latest_activity(driver):
+	actvities_candidate = driver.find_elements_by_class_name("usernav-activities")
+	try:
+		actvities_candidate[0].click()
+	except:
+		print "Could not click :'( "
+	# Actually we can go to the most recent history and use the left navigation. The most recent history is just the history where we cannot right navigate
+	still_unimported = True
+	while(still_unimported):
+		# unused for the moment though might be usefull summary_table = driver.find_elements_by_class_name("id")
+		candidates = driver.find_elements_by_partial_link_text("Course")
+		for cand in candidates:
+			upper = cand.find_element_by_xpath("../..")
+			if upper:
+				raw_infos = upper.get_attribute("class")
+				if "year" in raw_infos and "id" in raw_infos and "month" in raw_infos and "day" in raw_infos:
+					# print "found interesting activity : ", raw_infos
+					year_infos = int(raw_infos.split("year_")[1].split()[0])
+					month_infos = int(raw_infos.split("month_")[1].split()[0])
+					day_infos = int(raw_infos.split("day_")[1].split()[0])
+					found_date = CustDate(year = year_infos, month = month_infos, day = day_infos)
+					if found_date > last_strava_activity:
+						print "activity to add, dated: ", found_date
+						# The following opens a new tab, which originally was supposed to be better for navigation - one new tab for each activity. This is no longer what's considered but we keep the original "new window"
+						curr_w_h = driver.current_window_handle
+						cand.send_keys(Keys.CONTROL + Keys.RETURN)
+						driver.find_element_by_tag_name("body").send_keys(Keys.CONTROL + Keys.TAB)
+						driver.switch_to_window(curr_w_h) # switch focus to current window, ie. the window that has just been opened
+						break 
+		## TODO: decide if we have to keep on browsing history - and in that case actually browse it - or if we have reached the end
+		still_unimported = False
+		
+	can_go_later = True
+	while(can_go_later):
+		right_nav_buttons = driver.find_elements_by_class_name("nav_right")
+		if right_nav_buttons:
+			right_nav_buttons[0].click()
+		else:
+			can_go_later = False
 
-if last_strava_activity:
-	print last_strava_activity
+	del can_go_later
 	
-
-dl_dir = os.getcwd() + "\\" +"_".join(time.asctime().replace(":", "_").split())
-print dl_dir 
-
-profile = webdriver.FirefoxProfile()
-profile.set_preference("browser.download.folderList", 2)
-profile.set_preference("browser.download.dir", dl_dir)
-profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/gpx+tcx");
-profile.set_preference("browser.helperApps.alwaysAsk.force", False);
-profile.set_preference("browser.download.manager.showWhenStarting", False);
-
-
-driver = webdriver.Firefox(firefox_profile=profile)
-
-driver.get("https://www.runtastic.com")
-
-# Simple check on the website we've landed on
-assert "Runtastic" in driver.title or "runtastic" in driver.title
-print driver.title
-#time.sleep(1)
-
-# Detect "connect" button
-connect_btn_candidates = driver.find_elements_by_class_name("bttn")
-for candidate in connect_btn_candidates:
-    if "connect" in candidate.text and candidate.is_displayed():
-        candidate.click()
-        break
-
-########## Go through first page and connect to account ########## 
-# Detect fields to fill : email input field and password input field
-input_fields = driver.find_elements_by_class_name("js-input")
-mail_field_found = False
-pw_field_found = False
-for elem in input_fields:
-    parent = elem.find_element_by_xpath("..")
-    parent_class = parent.get_attribute("class")
-    if "email" in parent_class and "login" in parent_class:
-        mail_input_field = elem
-        mail_field_found = True
-    elif "password" in parent_class and "login" in parent_class:
-        pw_input_field = elem
-        pw_field_found = True
-    else:
-        print ""
-
-# Fill fields and connect to session
-if mail_field_found and pw_field_found:
-    mail_input_field.send_keys(runtastic_login)
-    pw_input_field.send_keys(runtastic_pw)
-    pw_input_field.send_keys(Keys.RETURN)
-
-########## ########## ########## ########## ########## ##########
-
-##########  Go through second page to activities history ##########
-# time.sleep(1)
-driver.implicitly_wait(8) # TODO: try explicit wait, using Expected conditions cf http://selenium-python.readthedocs.org/en/latest/waits.html
-actvities_candidate = driver.find_elements_by_class_name("usernav-activities")
-try:
-    actvities_candidate[0].click()
-except:
-    print "Could not click :'( "
-        
-# Actually we can go to the most recent history and use the left navigation. The most recent history is just the history where we cannot right navigate
-still_unimported = True
-while(still_unimported):
-	# unused for the moment though might be usefull summary_table = driver.find_elements_by_class_name("id")
-	candidates = driver.find_elements_by_partial_link_text("Course")
-	for cand in candidates:
-		upper = cand.find_element_by_xpath("../..")
-		if upper:
-			raw_infos = upper.get_attribute("class")
-			if "year" in raw_infos and "id" in raw_infos and "month" in raw_infos and "day" in raw_infos:
-				# print "found interesting activity : ", raw_infos
-				year_infos = int(raw_infos.split("year_")[1].split()[0])
-				month_infos = int(raw_infos.split("month_")[1].split()[0])
-				day_infos = int(raw_infos.split("day_")[1].split()[0])
-				found_date = CustDate(year = year_infos, month = month_infos, day = day_infos)
-				if found_date > last_strava_activity:
-					print "activity to add, dated: ", found_date
-					# The following opens a new tab, which originally was supposed to be better for navigation - one new tab for each activity. This is no longer what's considered but we keep the original "new window"
-					curr_w_h = driver.current_window_handle
-					cand.send_keys(Keys.CONTROL + Keys.RETURN)
-					driver.find_element_by_tag_name("body").send_keys(Keys.CONTROL + Keys.TAB)
-					driver.switch_to_window(curr_w_h) # switch focus to current window, ie. the window that has just been opened
-					break 
-	## TODO: decide if we have to keep on browsing history - and in that case actually browse it - or if we have reached the end
-	still_unimported = False
+	return
 	
-print "out of the break"
+def download_relevant_activities(driver, last_strava_activity):
+	date_in_range = True
+	while (date_in_range):
+		act_date_components = driver.title.split("(")[1].split("|")[0].split() # Warning: this is heavily dependant on runtastic way of displaying things
+		year_current = int(act_date_components[2])
+		processed_month = act_date_components[1].strip(",")
+		month_current = month_conversion[processed_month]
+		day_current = int(act_date_components[0])
+		act_date = CustDate(year = year_current, month = month_current, day = day_current)
+		if act_date > last_strava_activity:
+			browse_buttons_and_navigate(driver)
+			# options_buttons = driver.find_elements_by_id("show_more_options")
+			# for btn in options_buttons:
+				# if btn.get_attribute("class") == "" and btn.is_displayed():
+					# btn.click()
+					# childs = btn.find_elements_by_xpath(".//a")
+					# for elem in childs:
+						# if "charger" in elem.text and elem.is_displayed():
+							# print "let's dl"
+							# elem.click()
+							# driver.find_elements_by_partial_link_text(".tcx")[0].click()
+							# driver.find_elements_by_class_name("nav_left")[0].click()
+							# break ##TODO: get out of the multiple loops properly 
+		else:
+			date_in_range = False
+	
+if __name__ == "__main__":					
+	passed_args = sys.argv
+	date_components = passed_args[1:]
+	for s in sys.argv:
+	#    print s
+		pass
+		
+	last_strava_activity = 0
+	if len(date_components) == 3:
+		last_strava_activity = CustDate(year = int(date_components[0]), month = int(date_components[1]), day = int(date_components[2]))
 
-can_go_later = True
-while(can_go_later):
-	right_nav_buttons = driver.find_elements_by_class_name("nav_right")
-	if right_nav_buttons:
-		right_nav_buttons[0].click()
-	else:
-		can_go_later = False
+	if last_strava_activity:
+		print last_strava_activity
+		
+	driver = instantiate_auto_download_browser()
 
-del can_go_later
+	# Simple check on the website we've landed on
+	assert "Runtastic" in driver.title or "runtastic" in driver.title
+	# print driver.title
+	# time.sleep(1)
+	
+	log_onto_runtastic(driver)
 
-date_in_range = True
-while (date_in_range):
-	act_date_components = driver.title.split("(")[1].split("|")[0].split() # Warning: this is heavily dependant on runtastic way of displaying things
-	year_current = int(act_date_components[2])
-	processed_month = act_date_components[1].strip(",")
-	month_current = month_conversion[processed_month]
-	day_current = int(act_date_components[0])
-	act_date = CustDate(year = year_current, month = month_current, day = day_current)
-	if act_date > last_strava_activity:
-		browse_buttons_and_navigate(driver)
-		# options_buttons = driver.find_elements_by_id("show_more_options")
-		# for btn in options_buttons:
-			# if btn.get_attribute("class") == "" and btn.is_displayed():
-				# btn.click()
-				# childs = btn.find_elements_by_xpath(".//a")
-				# for elem in childs:
-					# if "charger" in elem.text and elem.is_displayed():
-						# print "let's dl"
-						# elem.click()
-						# driver.find_elements_by_partial_link_text(".tcx")[0].click()
-						# driver.find_elements_by_class_name("nav_left")[0].click()
-						# break ##TODO: get out of the multiple loops properly 
-	else:
-		date_in_range = False
-				
-
+	########## ########## ########## ########## ########## ##########
+	##########  Go through second page to activities history ##########
+	# time.sleep(1)
+	driver.implicitly_wait(8) # TODO: try explicit wait, using Expected conditions cf http://selenium-python.readthedocs.org/en/latest/waits.html
+	navigate_to_latest_activity(driver)
+	download_relevant_activities(driver, last_strava_activity)
+	
 ########## This is the code graveyard, in order to come back from the dead when the time shall be right ########## 
 
 ##for elem in elems_list:
